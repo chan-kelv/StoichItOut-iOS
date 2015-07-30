639b4d87a898923e677ecaa7cc0ad52560f0e068
Kelvin Chan <kelvin.chan@resaas.com>
2015-07-30 10:21:35 -0700

    Initial Commit

---
+++ StoichItOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoichItOut
+//
+//  Created by Dev on 2015-07-30.
+//  Copyright (c) 2015 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
